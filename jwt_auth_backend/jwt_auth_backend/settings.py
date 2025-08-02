bd9cb4b354882c421f0bdd2fbe07c03c288e6213
chitharthan-stack <sidhupkc@gmail.com>
2025-08-02 11:15:17 +0530

    requirements text update

--- jwt_auth_backend/jwt_auth_backend/settings.py
+++ jwt_auth_backend/jwt_auth_backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-exq^jd)dm5n+rei_6w2z2^+$2v)d(hmd%!mhkt(n1+8rglqa1*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
